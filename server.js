ff95299c8aa68a4486028a939cf8e86527111d16
raeganmb <raeganboucher2@gmail.com>
2022-10-24 10:39:41 -0300

    OAuth Steps 2-4

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
 
 // load the env vars
 require("dotenv").config();
@@ -21,6 +23,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "Raeganmb",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
